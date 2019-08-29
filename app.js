19151158d0bf7fcad627e647ba4d2607ee34936e
CN105 <cn105@CN0105.local>
2019-08-29 15:11:57 +0100

    function for drum created but single drum sound. css transition to fix too

--- app.js
+++ app.js
@@ -8,69 +8,76 @@ const snare = document.getElementById('snare')
 const tink = document.getElementById('tink')
 const tom = document.getElementById('tom')
 
-const body = document.querySelector('body')
+// const body = document.querySelector('body')
 
 
 // Play drum by keyboard press
 
 const sound = new Audio();  
 
+
+// every drum has a sound, a style
+// also may wish to change the overall drum kit
+
+// class noise {
+//     constructor(drumType){
+//         this.drumType = drumType
+//     }
+
+// }
+
+const makeDrum = (drum) =>{
+    console.log(`key pressed --> ${drum}`)
+
+    sound.src = `./sounds/${drum}.wav`
+    sound.play()
+    // `${drum}`.style.backgroundColor = "red"
+    // `${drum}`.style.color = "white"
+
+    boom.classList.toggle("bang")  // TEMP: only for boom at the mo !
+    
+}
+
+
+
+
 document.addEventListener("keydown", (event) => {
 
         switch(event.key) {
             case "q":
-                    console.log(`q pressed --> boom`)
-                    sound.src = './sounds/boom.wav'
-                    sound.play()
+                    makeDrum('boom')
             break;
 
             case "w":
-                    console.log(`w pressed --> clap`)
-                    sound.src = './sounds/clap.wav'
-                    sound.play()
+                    makeDrum('clap')
             break;
 
             case "e":
-                    console.log(`e pressed --> hihat`)
-                    sound.src = './sounds/hihat.wav'
-                    sound.play()
-
+                    makeDrum('clap')
             break;
 
             case "r":
-                    console.log(`r pressed --> kick`)
-                    sound.src = './sounds/kick.wav'
-                    sound.play()
+                    makeDrum('kick')
             break;
 
             case "t":
-                    console.log(`t pressed --> openhat`)
-                    sound.src = './sounds/openhat.wav'
-                    sound.play()
+                    makeDrum('openhat')
             break;
 
             case "y":
-                    console.log(`y pressed --> ride`)
-                    sound.src = './sounds/ride.wav'
-                    sound.play()
+                    makeDrum('ride')
             break;
 
             case "u":
-                    console.log(`u pressed --> snare`)
-                    sound.src = './sounds/snare.wav'
-                    sound.play()
+                    makeDrum('snare')
             break;
 
             case "i":
-                    console.log(`i pressed --> tink`)
-                    sound.src = './sounds/tink.wav'
-                    sound.play()
+                    makeDrum('tink')
             break;
 
             case "o":
-                    console.log(`o pressed --> tom`)
-                    sound.src = './sounds/tom.wav'
-                    sound.play()
+                    makeDrum('tom')
             break;
 
             default:
